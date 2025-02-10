277055ec5cd6ddd2f3a57d5e378249ba63ce3439
pedrumond <pe.drumond97@gmail.com>
2025-02-10 20:52:05 -0300

    feat: create cards routes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const user = require("./routes/users");
+const cards = require("./routes/cards");
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
 app.use("/", user);
+app.use("/", cards);
 
 app.listen(PORT, () => {
   console.log(`App executando na porta ${PORT}`);
