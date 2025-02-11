404e3689c0eba3742af0516f7dac1c7b6dcf5088
pedrumond <pe.drumond97@gmail.com>
2025-02-10 21:50:03 -0300

    feat: create route notFound

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
+
 const user = require("./routes/users");
 const cards = require("./routes/cards");
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
+const notFound = (req, res, next) => {
+  res.status(404).send({ message: "A solicitação não foi encontrada" });
+};
+
 app.use("/", user);
 app.use("/", cards);
+app.use("*", notFound);
 
 app.listen(PORT, () => {
   console.log(`App executando na porta ${PORT}`);
