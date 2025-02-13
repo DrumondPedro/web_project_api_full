81b97891749f1381afb317c444a51ac7f93c546c
pedrumond <pe.drumond97@gmail.com>
2025-02-13 19:40:44 -0300

    feat: add file reading

--- routes/users.js
+++ routes/users.js
@@ -5,31 +5,23 @@ import path from "node:path";
 const __dirname = import.meta.dirname;
 const user_router = Router();
 
-const usersData = [
-  {
-    name: "Ada Lovelace",
-    about: "Mathematician, writer",
-    avatar:
-      "https://www.biography.com/.image/t_share/MTE4MDAzNDEwODQwOTQ2MTkw/ada-lovelace-20825279-1-402.jpg",
-    _id: "dbfe53c3c4d568240378b0c6",
-  },
-  {
-    name: "Tim Berners-Lee",
-    about: "Inventor, scientist",
-    avatar:
-      "https://media.wired.com/photos/5c86f3dd67bf5c2d3c382474/4:3/w_2400,h_1800,c_limit/TBL-RTX6HE9J-(1).jpg",
-    _id: "d285e3dceed844f902650f40",
-  },
-  {
-    name: "Grace Hopper",
-    about: "Computer scientist",
-    avatar:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg/800px-Commodore_Grace_M._Hopper%2C_USN_%28covered%29.jpg",
-    _id: "7d8c010a1c97ca2654997a95",
-  },
-];
+var usersData = [];
+
+const filePath = path.join(__dirname, "..", "data", "users.json");
+
+fs.readFile(filePath, (err, data) => {
+  if (err) {
+    console.log(`Arquivo não encontrado. ${err}`);
+    return;
+  }
+  usersData = JSON.parse(data);
+});
 
 const sendAllUsers = (req, res, next) => {
+  if (usersData.length === 0) {
+    res.status(404).send({ message: "Arquivo não encontrado" });
+    return;
+  }
   res.send(usersData);
 };
 
