cae7ae6770a2ce2f5c01ac5b74a4e53fb331ca7e
pedrumond <pe.drumond97@gmail.com>
2025-03-08 20:22:11 -0300

    feat: Add shorter timeout for connection attempt

--- data/database.js
+++ data/database.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 function connectDatabase() {
   mongoose
-    .connect("mongodb://localhost:27017/arounddb")
+    .connect("mongodb://localhost:27017/arounddb", {
+      serverSelectionTimeoutMS: 2000,
+    })
     .then(() => console.log("MongoDB conectado com sucesso"))
     .catch((err) => console.log("Erro de conexão:", err));
 }
